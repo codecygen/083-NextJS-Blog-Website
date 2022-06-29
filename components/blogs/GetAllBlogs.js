4f49eab7c6fd3714dfccb550a956698d47007a39
gingercook <vahitaras@gmail.com>
2022-06-28 22:26:34 -0400

    database connection established

--- components/blogs/GetAllBlogs.js
+++ components/blogs/GetAllBlogs.js
@@ -2,21 +2,8 @@ import fetchLink from '../../data/dummy-database.json';
 
 const GetAllBlogs = (props) => {
   return (
-    <div>{props.data}</div>
+    <div>aras</div>
   );
-};
-
-export const getStaticProps = async () => {
-    console.log('aras');
-    const data = fetchLink.json();
-    console.log(data);
-    console.log('aras');
-
-    return {
-        props: {
-          data: data  
-        }
-    };
 };
 
 export default GetAllBlogs;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,32 @@
-import GetAllBlogs from "../components/blogs/GetAllBlogs";
+import GetAllBlogs from '../components/blogs/GetAllBlogs';
+import { MongoClient } from 'mongodb';
 
-const HomePage = () => {
+const HomePage = (props) => {
   return (
     <>
       <h1>Home Page</h1>
       <GetAllBlogs />
     </>
   );
+};
+
+export const getStaticProps = async () => {
+  const mongoAtlasConnectionLink = process.env.MONGODB_ATLAS_LINK;
+
+  const connection = await MongoClient.connect(mongoAtlasConnectionLink);
+  const db = connection.db();
+
+  const postsCollection = db.collection('posts');
+  const fetchedPosts = await postsCollection.find().toArray();
+  connection.close();
+
+  console.log(fetchedPosts);
+
+  return {
+      props: {
+        posts: 'aras' 
+      }
+  };
 };
 
 export default HomePage;
