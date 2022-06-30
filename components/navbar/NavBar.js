47fb34006b92a4ad287404d79836bf2a388ecea4
gingercook <vahitaras@gmail.com>
2022-06-30 00:23:31 -0400

    button finished

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Button from '../ui/Button';
 
 import classes from './NavBar.module.scss';
@@ -6,7 +7,19 @@ const NavBar = () => {
   return (
     <nav className={classes.navbar}>
       <h1>Blog Website</h1>
-      <Button>Write Blog</Button>
+      <div>
+        <Button>
+          <Link href="/">
+            All Blogs
+          </Link>
+        </Button>
+
+        <Button>
+          <Link href="/new-post">
+            Write Post
+          </Link>
+        </Button>
+      </div>
     </nav>
   );
 };
