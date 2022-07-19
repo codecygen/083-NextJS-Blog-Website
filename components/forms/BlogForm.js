9d65fbd236320497132ccfc9762adbd4faeb1c9f
gingercook <vahitaras@gmail.com>
2022-07-18 22:53:16 -0400

    write post section is done, submit button console logs the output correctly.

--- components/forms/BlogForm.js
+++ components/forms/BlogForm.js
@@ -1,10 +1,32 @@
+import { useRef } from 'react';
+
+import Button from '../ui/Button';
+
 import classes from './BlogForm.module.scss';
 
 const BlogForm = () => {
-  return (
-    <section className={classes['form-card']} id='blog'>
+    const titleRef = useRef();
+    const descriptionRef = useRef();
+    const pictureRef = useRef();
+    const contentRef = useRef();
+
+    const blogSubmitHandler = event => {
+        event.preventDefault();
+
+        const enteredData = {
+            title: titleRef.current.value,
+            description: descriptionRef.current.value,
+            picture: pictureRef.current.value,
+            content: contentRef.current.value
+        };
+
+        console.log(enteredData);
+    }
+
+    return (
+        <section className={classes['form-card']} id='blog'>
             <div>
-                <form>
+                <form onSubmit={blogSubmitHandler}>
                     <h3>Write a New Blog</h3>
                     <div className={classes['input-div']}>
                         <label htmlFor='title'>Title</label>
@@ -12,6 +34,7 @@ const BlogForm = () => {
                             type='text'
                             id='title'
                             placeholder='Blog Title...'
+                            ref={titleRef}
                         />
                     </div>
 
@@ -21,6 +44,17 @@ const BlogForm = () => {
                             type='text'
                             id='description'
                             placeholder='Blog Description...'
+                            ref={descriptionRef}
+                        />
+                    </div>
+
+                    <div className={classes['input-div']}>
+                        <label htmlFor='picture'>Picture</label>
+                        <input
+                            type='text'
+                            id='picture'
+                            placeholder='Picture Path...'
+                            ref={pictureRef}
                         />
                     </div>
 
@@ -30,14 +64,17 @@ const BlogForm = () => {
                             type='text'
                             id='content'
                             placeholder='Blog Content...'
+                            ref={contentRef}
                         />
                     </div>
 
-                    {/* <Button><a href>Shoot!</a></Button> */}
+                    <Button buttonStyle="2">
+                        Submit
+                    </Button>
                 </form>
             </div>
         </section>
-  );
+    );
 };
 
 export default BlogForm;
